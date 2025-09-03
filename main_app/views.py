afc228a6fc5f89e68b9e6fcfa25879dc85c02db8
ayahmeftah <ayahmeftah123123@gmail.com>
2025-09-03 21:25:55 +0300

    created function for the selected templated to be saved and redirect to the forms

--- main_app/views.py
+++ main_app/views.py
@@ -10,6 +10,7 @@ from . import models, forms
 class HomePageView(TemplateView):
     template_name = "homepage.html"
 
+
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = "registration/signup.html"
@@ -19,9 +20,23 @@ class SignUpView(CreateView):
         user = form.save()
         login(self.request, user)
         return redirect(self.success_url)
-    
-    
+
+
 class TemplateListView(ListView):
     model = models.Template
     template_name = "resumes/template_list.html"
     context_object_name = "templates"
+
+
+def select_template(request, template_id):
+    template = models.Template.objects.get(pk=template_id)
+    resume = models.Resume.objects.create(
+        user=request.user,
+        template=template,
+        resume_name= "",
+        candidate_name=request.user.get_full_name() or request.user.username,
+        email=request.user.email,
+        mobile=""
+        )
+    
+    return redirect('resume_personal_info', resume_id=resume.id)
