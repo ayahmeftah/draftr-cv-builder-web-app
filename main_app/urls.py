e5068ccffa24d41d5f153ed6c026c337d064eb77
ayahmeftah <ayahmeftah123123@gmail.com>
2025-09-05 13:29:49 +0300

    created eduction form html with usrl for adding

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,8 +5,12 @@ from . import views
 urlpatterns = [
     path("",views.HomePageView.as_view(),name="homepage"),
     path("auth/signup/", views.SignUpView.as_view(), name="signup"),
+    
     path("resumes/", views.TemplateListView.as_view(), name="template_list"),
     path("resumes/select/<int:template_id>/", views.select_template, name="select_template"),
     path("resumes/<int:resume_id>/personal/", views.PersonalInfoView.as_view(), name="resume_personal_info"),
     path("resumes/<int:resume_id>/preview/", views.ResumePreviewView.as_view(), name="resume_preview"),
+    
+    # Education
+    path("resume/<int:resume_id>/education/add/", views.EducationCreateView.as_view(), name="education_add"),
 ]
