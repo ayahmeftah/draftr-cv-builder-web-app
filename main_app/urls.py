fd6b5268604688ae6504724944962855d8cba4ac
ayahmeftah <ayahmeftah123123@gmail.com>
2025-09-06 16:16:25 +0300

    added loader for the build just for looks

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path("",views.HomePageView.as_view(),name="homepage"),
     path("auth/signup/", views.SignUpView.as_view(), name="signup"),
     
-    path("resumes/", views.TemplateListView.as_view(), name="template_list"),
-    path("resumes/select/<int:template_id>/", views.select_template, name="select_template"),
+    path("resumes/"),
+    path("resumes/templates/", views.TemplateListView.as_view(), name="template_list"),
+    path("resumes/templates/<int:template_id>/", views.select_template, name="select_template"),
     path("resumes/<int:resume_id>/personal/", views.PersonalInfoView.as_view(), name="resume_personal_info"),
     path("resumes/<int:resume_id>/preview/", views.ResumePreviewView.as_view(), name="resume_preview"),
     
