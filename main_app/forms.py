9d12aee013228774371ffacf04edb01ffa0e830a
ayahmeftah <ayahmeftah123123@gmail.com>
2025-09-09 10:51:55 +0300

    added first name and last name to the user creation form

--- main_app/forms.py
+++ main_app/forms.py
@@ -7,10 +7,12 @@ from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("first_name","last_name","username", "email", "password1", "password2")
 
     def clean_email(self):
         email = self.cleaned_data.get("email")
@@ -22,6 +24,8 @@ class CustomUserCreationForm(UserCreationForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
         if commit:
             user.save()
         return user
